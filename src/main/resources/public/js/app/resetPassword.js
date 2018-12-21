2c8eb5f466331ee6bbd4ddcd2f1893dba9b786e9
Manoj Mandal <manoj@insonix.com>
2018-12-21 18:00:12 +0530

    /resolve #20 Validations & UI chages on Forgot Password Screens done

--- src/main/resources/public/js/app/resetPassword.js
+++ src/main/resources/public/js/app/resetPassword.js
@@ -1,13 +1,16 @@
 $("#forgotPasswordSubmit").attr('disabled', true);
-$("#username").attr('disabled', true);
+$("#username").prop("readonly", true);
+
 
 var resetPassword = {
 
 	// function for validating password for reset the password.
 	validatePassword : function(e) {
-		var password = $("#newPassword").val();
-		var confirm_password = $("#confirmPassword").val();
-         //compare password 
+
+		let password = $("#newPassword").val();
+		let confirm_password = $("#confirmPassword").val();
+
+		// compare password 
 		if (password != confirm_password) {
 			$(".errorMsg").text('Passwords Do Not Match.');
 			$(".errorMsg").css('color', 'red');
@@ -15,11 +18,40 @@ var resetPassword = {
 			return false;
 		} else {
 			$(".errorMsg").text('');
-			$('.confirmPassword').css('border-color', '');
-			$("#forgotPasswordSubmit").attr('disabled', false);
-
+			enableSubmitButton();
 		}
 		return true;
+
+	},
+	validateOTP : function(e) {
+		let otp = $("#otp").val();
+		if (otp == null || otp == "" || otp == undefined) {
+			$(".otpErrorMsg").text('enter valid otp.');
+			$(".otpErrorMsg").css('color', 'red');
+			$("#forgotPasswordSubmit").attr('disabled', true);
+		} else {
+			$(".otpErrorMsg").text('');
+			enableSubmitButton();
+
+		}
+
 	}
-	
+
+}
+
+// Common Method to check Enable/Disable The Submit Button
+function enableSubmitButton() {
+	let password = $("#newPassword").val();
+	let confirm_password = $("#confirmPassword").val();
+	let otp = $("#otp").val();
+	if (password !== null && password !== "" && password !== undefined
+			&& confirm_password !== null && confirm_password !== ""
+			&& confirm_password !== undefined && otp !== null && otp !== ""
+			&& otp !== undefined) {
+
+		$("#forgotPasswordSubmit").attr('disabled', false);
+
+	} else {
+		$("#forgotPasswordSubmit").attr('disabled', true);
+	}
 }
