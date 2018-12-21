c366baee3f331ddc187e13381dc38b664463c8af
Nikhil Mahajan <nik10.mah@gmail.com>
2018-12-21 11:21:12 +0530

    Added Unit test case for Node js application

---
+++ src/test/java/com/ml/epic/ta/TwitterStreamTest.java
@@ -0,0 +1,47 @@
+package com.ml.epic.ta;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class TwitterStreamTest extends BaseTest {
+
+	private static String BASE_URL = "http://localhost:9000/api";
+
+	@Test
+	public void test() throws URISyntaxException {
+		RestTemplate restTemplate = new RestTemplate();
+
+		URI uri = new URI(BASE_URL);
+
+		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+		System.out.println("Response ===> " + result.getBody());
+		// Verify request succeed
+		Assert.assertEquals(200, result.getStatusCodeValue());
+		Assert.assertEquals(true, result.getBody().contains("message"));
+	}
+
+	@Test
+	public void start() throws URISyntaxException {
+		RestTemplate restTemplate = new RestTemplate();
+
+		String url = BASE_URL + "/event/start";
+		String requestJson = "{\"event_name\":\"NewYearEve2019\",\"topics\":[\"party\",\"festa\",\"partido\",\"بركان\",\"كارثة\",\"furacão\",\"desastre\"],\"languages\":[\"en\",\"ar\",\"pt\",\"es\"]}";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
+		String resp = restTemplate.postForObject(url, entity, String.class);
+		System.out.println(resp);
+
+	}
+
+}
